cd273d031b2183158c42dffabe2ae896957c1969
rattling <curry.john@gmail.com>
2025-06-03 12:14:06 +0100

    chapter 3 Adding a sidebar

--- mega/react-microblog/src/App.js
+++ mega/react-microblog/src/App.js
@@ -17,16 +17,6 @@ function renderPost(post) {
   return <Post key={post.id} post={post} />;
 }
 
-function HorizontalExample() {
-  return (
-    <Stack direction="horizontal" gap={3}>
-      <div className="p-2">First item</div>
-      <div className="p-2">Second item</div>
-      <div className="p-2">Third item</div>
-    </Stack>
-  );
-}
-
 export default function App() {
   const posts = [
     {
--- mega/react-microblog/src/components/Sidebar.js
+++ mega/react-microblog/src/components/Sidebar.js
@@ -1,3 +1,14 @@
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 export default function Sidebar() {
-  return <p>sidebar</p>;
+  return (
+    <Navbar sticky="top" className="flex-column Sidebar">
+      <Nav.Item>
+        <Nav.Link href="/">Feed</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/">Explore</Nav.Link>
+      </Nav.Item>
+    </Navbar>
+  );
 }
